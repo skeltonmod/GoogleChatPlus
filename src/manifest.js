646e8ed3801c9f30cfb4353cab9763c4c5f29807
Amogus <elijah.abgao@fligno.com>
2023-04-19 22:25:15 +0800

    Replace logos and locales

--- src/manifest.js
+++ src/manifest.js
@@ -15,7 +15,6 @@ export default defineManifest({
     default_popup: 'popup.html',
     default_icon: 'img/logo-48.png',
   },
-  "default_locale": "en",
   options_page: 'options.html',
   background: {
     service_worker: 'src/background/index.js',
